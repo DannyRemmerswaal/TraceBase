36897db90e91e8e2c334a2baa2a2879de3695c3d
Danny Remmerswaal <dannyremmerswaal@gmail.com>
2016-11-07 13:14:47 +0100

    Added DB connection

--- TraceBase/TraceBase/settings.py
+++ TraceBase/TraceBase/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'TraceBase.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'TraceBase',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -107,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC+1'
 
 USE_I18N = True
 
