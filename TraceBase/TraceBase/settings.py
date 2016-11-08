9cdae8d3f6d860af6c02efcc22e7123804b9b068
Danny Remmerswaal <dannyremmerswaal@gmail.com>
2016-11-08 09:48:11 +0100

    Fixed local webserver

--- TraceBase/TraceBase/settings.py
+++ TraceBase/TraceBase/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'TraceBaseDjango',
+    'TraceBase',
 ]
 
 MIDDLEWARE = [
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC+1'
+TIME_ZONE = 'Europe/Amsterdam'
 
 USE_I18N = True
 
